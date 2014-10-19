37ac80c1d7cd396831e131b2b52e0ff9534c88a6
Johan Angelstam <johan@angelstam.se>
2014-10-19 17:19:20 +0200

    Update DR-TV service with support for HLS streams.

--- lib/pirateplay/lib/services/dr.py
+++ lib/pirateplay/lib/services/dr.py
@@ -1,8 +1,13 @@
 from ..rerequest import TemplateRequest
 from common import redirect_handler
 
-services = [{ 'title': 'DR-TV', 'url': 'http://dr.dk/tv',
-				'items': [TemplateRequest( re = r'(?P<req_url>(http://)?(www\.)?dr\.dk/.+)' ),
+init_req = TemplateRequest( re = r'(?P<req_url>(http://)?(www\.)?dr\.dk/.+)' )
+
+
+#Request chains
+
+rtmp = { 'title': 'DR-TV', 'url': 'http://dr.dk/tv',
+				'items': [init_req,
 						TemplateRequest(
 							re = r'videoData:\s+{.+?resource:\s+"(?P<req_url>[^"]+)"',
 							handlerchain = redirect_handler()),
@@ -11,4 +16,20 @@ services = [{ 'title': 'DR-TV', 'url': 'http://dr.dk/tv',
 							re = r'"uri":"(?P<rtmp_base>rtmpe?:\\/\\/vod\.dr\.dk\\/cms\\/)(?P<rtmp_path>[^"]+).*?"bitrateKbps":(?P<bitrate>\d+)',
 							encode_vars = lambda v: { 'final_url': ('%(rtmp_base)s playpath=%(rtmp_path)s swfVfy=1 swfUrl=http://www.dr.dk/assets/swf/program-player.swf' % v).replace('\\', ''),
 														'quality': '%(bitrate)s kbps' % v,
-														'suffix-hint': 'flv' })] }]
+														'suffix-hint': 'flv' })] }
+
+hls = { 'items': [init_req,
+						TemplateRequest(
+							re = r'data-resource="(?P<req_url>[^"]+)"',
+							handlerchain = redirect_handler()),
+						TemplateRequest( re = r'Location: (?P<req_url>.*?)\n' ),
+						TemplateRequest(
+							re = r'"MimeType":"text/vtt;charset=utf-8",.*?"Uri":"(?P<subtitles>http?://[^"]+)'),
+						TemplateRequest(
+							re = r'"Uri":"(?P<url>http?://[^"]+).*?"FileFormat":"(?P<format>[^"]+).*?"Bitrate":(?P<bitrate>\d+)',
+							encode_vars = lambda v: { 'final_url': '%(url)s' % v,
+														'quality': '%s kbps' % (str(int(v['bitrate']))),
+														'suffix-hint': v['format'] })] }
+
+services = [rtmp,
+			hls]
