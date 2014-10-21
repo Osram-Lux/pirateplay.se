1a8c02dfefe18b14f9027fb7109dd757bf69e8b6
Johan Angelstam <johan@angelstam.se>
2014-10-21 19:46:16 +0200

    Update DR-TV service with support for another kind of HLS streams.

--- lib/pirateplay/lib/services/dr.py
+++ lib/pirateplay/lib/services/dr.py
@@ -3,6 +3,8 @@ from common import redirect_handler
 
 init_req = TemplateRequest( re = r'(?P<req_url>(http://)?(www\.)?dr\.dk/.+)' )
 
+stream_re = r'"Uri":"(?P<req_url>%s[^"]+\.%s[^"]*)"'
+
 
 #Request chains
 
@@ -30,6 +32,21 @@ hls = { 'items': [init_req,
 							encode_vars = lambda v: { 'final_url': '%(url)s' % v,
 														'quality': '%s kbps' % (str(int(v['bitrate']))),
 														'suffix-hint': v['format'] })] }
+hls2 = { 'items': [init_req,
+						TemplateRequest(
+							re = r'data-resource="(?P<req_url>[^"]+)"',
+							handlerchain = redirect_handler()),
+						TemplateRequest( re = r'Location: (?P<req_url>.*?)\n' ),
+						TemplateRequest(
+							re = r'"MimeType":"text/vtt;charset=utf-8",.*?"Uri":"(?P<subtitles>http?://[^"]+)'),
+						TemplateRequest(
+							re = stream_re % ('http://', 'm3u8')),
+						TemplateRequest(
+							re = r'BANDWIDTH=(?P<bitrate>\d+).*?RESOLUTION=(?P<resolution>\d+x\d+).*?(?P<url>https?://[^\n]+)',
+							encode_vars = lambda v: { 'final_url': '%(url)s' % v,
+														'quality': '%s kbps' % (str(int(v['bitrate'])/1000)),
+														'suffix-hint': 'mp4' })] }
 
 services = [rtmp,
-			hls]
+			hls,
+			hls2]
