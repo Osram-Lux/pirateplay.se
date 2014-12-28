1b05661721578907e3d6f2585ffef6075c162611
Johan Angelstam <johan@angelstam.se>
2014-12-28 20:33:12 +0100

    Update DR-TV to fix subtitle regexp.

--- lib/pirateplay/lib/services/dr.py
+++ lib/pirateplay/lib/services/dr.py
@@ -38,7 +38,7 @@ hls2 = { 'items': [init_req,
 							handlerchain = redirect_handler()),
 						TemplateRequest( re = r'Location: (?P<req_url>.*?)\n' ),
 						TemplateRequest(
-							re = r'"MimeType":"text/vtt;charset=utf-8",.*?"Uri":"(?P<subtitles>http?://[^"]+)'),
+							re = r'"MimeType":"text/vtt;charset=utf-8",.*?"Uri":"(?P<subtitles>https?://[^"]+)'),
 						TemplateRequest(
 							re = stream_re % ('http://', 'm3u8')),
 						TemplateRequest(
